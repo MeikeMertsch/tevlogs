e9362ab8b550155cfe143c031f047138d7676ba7
Meike Mertsch <nmmertsch@gmail.com>
2015-10-14 12:40:46 +0200

    added cheshire and time and parsed a single line to be json

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cheshire "5.5.0"]
+                 [clj-time "0.11.0"]]
   :main ^:skip-aot tevlogs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/tevlogs/core.clj
+++ src/tevlogs/core.clj
@@ -1,5 +1,24 @@
 (ns tevlogs.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [cheshire.core :as json])
   (:gen-class))
+
+(defn parse-line [line]
+  (-> line
+      (string/split #" : ")
+      last
+      (json/parse-string true)))
+
+(defn read-lines [file-name]
+  (with-open [rdr (io/reader file-name)]
+    (map parse-line (line-seq rdr))))
+
+(def example "I, [2015-10-14T09:53:22.248366 #18826]  INFO -- : {\"toilet_name\": \"male-left\", \"time\": \"2015-10-14T09:53:22.229182\", \"door_closed\": true, \"playing\": true, \"song\": \"/home/pi/Rugrats Theme Song.mp3\"}")
+
+(parse-line example)
+(read-lines "/Users/djastin/Downloads/events-state.log")
+
 
 (defn -main
   "I don't do a whole lot ... yet."
