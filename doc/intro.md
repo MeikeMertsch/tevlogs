7eba27f14b48c30aeb738e2950ea1f704671ce8c
Meike Mertsch <nmmertsch@gmail.com>
2015-10-14 12:08:11 +0200

    Initial import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tevlogs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tevlogs/core_test.clj
@@ -0,0 +1,7 @@
+(ns tevlogs.core-test
+  (:require [clojure.test :refer :all]
+            [tevlogs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
